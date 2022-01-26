d9c3a4c983afe4aec906ab4c48b726c5e711060e
shadeofblue <blue@wave460.net>
2022-01-27 00:44:07 +0100

    stop movement on bounds

--- game/Tank.gd
+++ game/Tank.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
 
-export var max_velocity = 100
+export var max_velocity: float = 100
+export var acceleration_impulse: float = 5
+export var deceleration_impulse: float = 1
+export var margin_deceleration_impulse: float = 10
+export var max_acceleration: float = 5
+export var rotation_speed = 3
 
 var velocity = Vector2.ZERO
-export var acceleration_impulse: float = 5
+var margin = 20
+var max_position: Vector2
 
 var acceleration_map: Dictionary
 
@@ -15,6 +21,10 @@ func init_acceleration_map():
 		"tank_up": Vector2(0, -acceleration_impulse),
 		"tank_down": Vector2(0, acceleration_impulse),
 	}
+
+func start(screen_size):
+	position = screen_size / 2
+	max_position = screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,7 +39,23 @@ func _process(delta):
 		if Input.is_action_pressed(key):
 			acceleration += acceleration_map[key]
 	
+	if position.x < margin and velocity.x < 0:
+		acceleration.x = margin_deceleration_impulse
+	if position.x > max_position.x - margin and velocity.x > 0:
+		acceleration.x = - margin_deceleration_impulse
+	
+	if position.y < margin and velocity.y < 0:
+		acceleration.y = margin_deceleration_impulse
+	if position.y > max_position.y - margin and velocity.y > 0:
+		acceleration.y = - margin_deceleration_impulse
+	
+	if acceleration.length() > max_acceleration:
+		acceleration = acceleration.normalized() * max_acceleration
+	
 	velocity += acceleration
 	
+	if velocity.length() > max_velocity:
+		velocity = velocity.normalized() * max_velocity
+	
 	position += velocity * delta
-
+	rotation_degrees += rotation_speed * delta

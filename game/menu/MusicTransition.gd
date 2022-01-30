46b61ab211d92310e962a437de391b6b8afdb70f
Kuba Mazurek <zakaprov@gmail.com>
2022-01-30 02:33:22 +0100

    Add music transition from title screen

---
+++ game/menu/MusicTransition.gd
@@ -0,0 +1,15 @@
+extends Node
+
+export(float) var volume_db = 0 setget set_volume
+
+func _ready():
+	$AnimationPlayer.play_backwards("FadeMusic")
+
+func transition():
+	$AnimationPlayer.play("FadeMusic")
+	yield($AnimationPlayer, "animation_finished")
+	$AnimationPlayer.play_backwards("FadeMusic")
+
+func set_volume(value):
+	print(value)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), -value)
--- game/menu/StartButton.gd
+++ game/menu/StartButton.gd
@@ -7,3 +7,4 @@ func _gui_input(event):
 	if event.pressed:
 		if event.is_action("ui_accept"):
 			get_node("../../SceneTransition").transition()
+			get_node("../../MusicTransition").transition()
